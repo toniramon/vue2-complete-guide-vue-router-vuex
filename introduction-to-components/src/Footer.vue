6fd391935b89d69d43d70ed980c044841a49961c
Web Development <info@toniramon.es>
2020-04-20 14:01:36 +0200

    components added

---
+++ introduction-to-components/src/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12">
+            <footer>
+                <p>All servers are manager here.</p>
+            </footer>
+        </div>
+    </div>
+</template>
---
+++ introduction-to-components/src/Servers.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul>
+            <li
+                class="list-group-item"
+                v-for="index in 5" v-bind:key="index">
+                Server #{{ index }}
+            </li>
+        </ul>
+    </div>
+</template>
