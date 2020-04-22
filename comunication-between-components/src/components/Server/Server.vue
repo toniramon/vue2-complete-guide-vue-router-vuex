aca59dd43cb4b2e6ac30b667aaa32afcdf68447e
Web Development <info@toniramon.es>
2020-04-22 13:27:26 +0200

    components task completed

---
+++ comunication-between-components/src/components/Server/Server.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                    class="list-group-item"
+                    style="cursor: pointer"
+                    @click="serverSelected">
+                Server #{{ server.id }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { serverBus } from '../../main';
+    export default {
+        props: ['server'],
+        methods: {
+            serverSelected() {
+                serverBus.$emit('serverSelected', this.server);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ comunication-between-components/src/components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Please select a Server</p>
+        <p v-else>Server #{{ server.id }} selected, Status: {{ server.status }}</p>
+        <hr>
+        <button @click="resetStatus">Change to Normal</button>
+    </div>
+
+</template>
+
+<script>
+    import { serverBus } from '../../main';
+
+    export default {
+        data: function() {
+            return {
+                server: null
+            }
+        },
+        created() {
+            serverBus.$on('serverSelected', (server) => {
+                this.server = server;
+            });
+        },
+        methods: {
+            resetStatus() {
+                this.server.status = 'Normal';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
